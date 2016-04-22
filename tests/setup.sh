1b2714b74738927cab17557655f790ec6336beb2
Nicholas Roberts <niccolo.roberts@gmail.com>
2016-04-22 10:01:43 -0700

    changed source Ansible setup to 2.0.0.1

--- tests/setup.sh
+++ tests/setup.sh
@@ -7,7 +7,7 @@
 #sudo rm -rf /var/lib/mysql
 #sudo rm -rf /root/.my.cnf
 
-curl https://raw.githubusercontent.com/GetValkyrie/ansible-bootstrap/master/install-ansible.sh | /bin/sh
+curl https://raw.githubusercontent.com/ErgonLogicEnterprises/ansible-bootstrap/master/install-ansible.sh | /bin/sh
 ansible-galaxy install --ignore-errors http://github.com/geerlingguy/ansible-role-mysql.git,,geerlingguy.mysql
 ansible-galaxy install --ignore-errors http://github.com/geerlingguy/ansible-role-drush.git,,geerlingguy.drush
 target=/etc/ansible/roles/ansible-role-aegir
